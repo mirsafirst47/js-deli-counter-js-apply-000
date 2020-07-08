8e73a53f21019bfb924b9befa2f1505029f76f19
Samir Triande <habib.triande@gmail.com>
2020-07-08 15:28:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+
+}
+
+function currentLine(katzDeliLine) {
+
+}
